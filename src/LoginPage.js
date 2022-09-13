bc92c9f7cd78523ae50e14320212a676549e0b2c
MikeDePacina <mikedpacina@gmail.com>
2022-09-12 18:05:00 -0700

    added login form

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Loginform from "./components/Loginform";
 const LoginPage = () => {
   return (
     <>
       <Navbar />
+      <Loginform />
       <p>Login page</p>
     </>
   );
